e4d4d0545539f867965009bdbdf4980b701ee572
vrndarani <98180585+vrndarani@users.noreply.github.com>
2022-06-16 16:22:19 +0530

    hometask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        int sum = 0;
+
+        for (int elm:arr){
+            sum += elm;
+        }
+        System.out.println("Monthly total spending is $" + sum);
+
+
+
+
+        int min = arr[0];
+        int max = arr[0];
+        for (final int elm : arr) {
+            if (elm < min) {
+                min = elm;
+            }
+            if (elm>max){
+                max = elm;
+            }
+        }
+        System.out.println("Smallest spending total is $" + min);
+        System.out.println("Biggest spending total is $" + max);
+
+        System.out.println("The average monthly spending is " + (float)sum/arr.length);
+
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i>=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
+
+
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
